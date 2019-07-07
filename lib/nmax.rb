e4cdc8f7b5227b0af3c77a8614f8b9cb524754dd
Dmitry Zvorykin <d.zvorikin@gmail.com>
2019-07-07 19:40:52 +0300

    MVP

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,6 +1,23 @@
+# frozen_string_literal: true
+
 require "nmax/version"
 
 module Nmax
-  class Error < StandardError; end
-  # Your code goes here...
+  INTEGERREGEXP = /\d+/.freeze
+
+  def self.perform(stream, integers_limit = 100, char_limit = 1000)
+    integers = []
+
+    stream.each(char_limit) do |line|
+      line_integers = line.scan(INTEGERREGEXP).map(&:to_i)
+
+      next if line_integers.empty?
+
+      integers = integers.concat(line_integers)
+                         .sort
+                         .take(integers_limit)
+    end
+
+    integers
+  end
 end
